5561f29535b172930898dd195ecb7178abc48185
eddyfk <56110411+EddyKazary@users.noreply.github.com>
2023-02-03 11:58:09 +0300

    Add files via upload
    creation git master

---
+++ G_paie_marin/src/main/java/com/ro/g_paie_marin/GPaieMarinApplication.java
@@ -0,0 +1,13 @@
+package com.ro.g_paie_marin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPaieMarinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPaieMarinApplication.class, args);
+	}
+
+}
---
+++ G_paie_marin/src/test/java/com/ro/g_paie_marin/GPaieMarinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ro.g_paie_marin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPaieMarinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
